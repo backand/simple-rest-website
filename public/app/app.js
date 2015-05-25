3b5a454a36bdde060187ed486cd993a49c349f6c
Itay Herskovits <itay@backand.com>
2015-05-25 14:07:52 +0300

    upgrade to version 1.6.0 of Backand SDK

--- public/app/app.js
+++ public/app/app.js
@@ -1,202 +1,170 @@
-angular.module('SimpleRESTWebsite', ['angular-storage', 'ui.router', 'backand')
-    .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-        $stateProvider
-            .state('login', {
-                url: '/login',
-                templateUrl: 'app/templates/login.tmpl.html',
-                controller: 'LoginCtrl',
-                controllerAs: 'login'
-            })
-            .state('dashboard', {
-                url: '/dashboard',
-                templateUrl: 'app/templates/dashboard.tmpl.html',
-                controller: 'DashboardCtrl',
-                controllerAs: 'dashboard'
-            });
+angular.module('SimpleRESTWebsite', ['angular-storage', 'ui.router', 'backand'])
 
-        $urlRouterProvider.otherwise('/dashboard');
-
-        $httpProvider.interceptors.push('APIInterceptor');
-    })
-    .service('APIInterceptor', function($rootScope) {
-        var service = this;
-
-
-        service.responseError = function(response) {
-            if (response.status === 401) {
-                $rootScope.$broadcast('unauthorized');
-            }
-            return response;
-        };
-    })
-    .service('UserService', function(store) {
-        var service = this,
-            currentUser = null;
-
-        service.setCurrentUser = function(user) {
-            currentUser = user;
-            store.set('user', user);
-            return currentUser;
-        };
-
-        service.getCurrentUser = function() {
-            if (!currentUser) {
-                currentUser = store.get('user');
-            }
-            return currentUser;
-        };
-    })
-    .service('LoginService', function($http) {
-        var service = this,
-            path = 'Users/';
-
-        function getUrl() {
-            return ENDPOINT_URI + path;
-        }
-
-        function getLogUrl(action) {
-            return getUrl() + action;
-        }
-
-        service.login = function(credentials) {
-            return $http.post(getLogUrl('login'), credentials);
-        };
-
-        service.logout = function() {
-            return $http.post(getLogUrl('logout'));
-        };
-
-        service.register = function(user) {
-            return $http.post(getUrl(), user);
-        };
-    })
-    .service('ItemsModel', function ($http, Backand) {
-        var service = this,
-            tableUrl = '/1/objects/',
-            path = 'items/';
-
-        function getUrl() {
-            return Backand.getApiUrl() + tableUrl + path;
-        }
-
-        function getUrlForId(itemId) {
-            return getUrl(path) + itemId;
-        }
-
-        service.all = function () {
-            return $http.get(getUrl());
-        };
-
-        service.fetch = function (itemId) {
-            return $http.get(getUrlForId(itemId));
-        };
-
-        service.create = function (item) {
-            return $http.post(getUrl(), item);
-        };
-
-        service.update = function (itemId, item) {
-            return $http.put(getUrlForId(itemId), item);
-        };
-
-        service.destroy = function (itemId) {
-            return $http.delete(getUrlForId(itemId));
-        };
-    })
-    .controller('LoginCtrl', function($rootScope, $state, Backand, UserService){
-        var login = this;
-
-        function signin() {
-            Backand.signin(login.email, login.password, login.appName)
-                .then(function(token) {
-                    $rootScope.$broadcast('authorized');
-                    $state.go('dashboard');
-                }, function(error) {
-                    console.log(error);
-                });
-        }
-
-        login.newUser = false;
-        login.signin = signin;
-    })
-    .controller('MainCtrl', function ($rootScope, $state, LoginService, Backand) {
-        var main = this;
-
-        function logout() {
-            Backand.signout();
-            $state.go('login');
-        }
-
-        $rootScope.$on('unauthorized', function() {
-            $state.go('login');
+.config(function(BackandProvider, $stateProvider, $urlRouterProvider, $httpProvider) {
+    BackandProvider.manageDefaultHeaders();
+    //BackandProvider.setAnonymousToken('Your Anonymous Token');
+    //BackandProvider.setSignUpToken('Your SignUp Token');
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/templates/login.tmpl.html',
+            controller: 'LoginCtrl',
+            controllerAs: 'login'
+        })
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'app/templates/dashboard.tmpl.html',
+            controller: 'DashboardCtrl',
+            controllerAs: 'dashboard'
         });
 
-        main.logout = logout;
-    })
-    .controller('DashboardCtrl', function(ItemsModel){
-        var dashboard = this;
+    $urlRouterProvider.otherwise('/dashboard');
 
-        function getItems() {
-            ItemsModel.all()
-                .then(function (result) {
-                    dashboard.items = result.data.data;
-                });
+    $httpProvider.interceptors.push('APIInterceptor');
+})
+.service('APIInterceptor', function($rootScope, $q) {
+    var service = this;
+
+
+    service.responseError = function(response) {
+        if (response.status === 401) {
+            $rootScope.$broadcast('unauthorized');
         }
+        return $q.reject(response);
+    };
+})
 
-        function createItem(item) {
-            ItemsModel.create(item)
-                .then(function (result) {
-                    initCreateForm();
-                    getItems();
-                });
-        }
+.service('ItemsModel', function ($http, Backand) {
+    var service = this,
+        tableUrl = '/1/objects/',
+        path = 'items/';
 
-        function updateItem(item) {
-            ItemsModel.update(item.Id, item)
-                .then(function (result) {
-                    cancelEditing();
-                    getItems();
-                });
-        }
+    function getUrl() {
+        return Backand.getApiUrl() + tableUrl + path;
+    }
 
-        function deleteItem(itemId) {
-            ItemsModel.destroy(itemId)
-                .then(function (result) {
-                    cancelEditing();
-                    getItems();
-                });
-        }
+    function getUrlForId(itemId) {
+        return getUrl(path) + itemId;
+    }
 
-        function initCreateForm() {
-            dashboard.newItem = { name: '', description: '' };
-        }
+    service.all = function () {
+        return $http.get(getUrl());
+    };
 
-        function setEditedItem(item) {
-            dashboard.editedItem = angular.copy(item);
-            dashboard.isEditing = true;
-        }
+    service.fetch = function (itemId) {
+        return $http.get(getUrlForId(itemId));
+    };
 
-        function isCurrentItem(itemId) {
-            return dashboard.editedItem !== null && dashboard.editedItem.Id === itemId;
-        }
+    service.create = function (item) {
+        return $http.post(getUrl(), item);
+    };
 
-        function cancelEditing() {
-            dashboard.editedItem = null;
-            dashboard.isEditing = false;
-        }
+    service.update = function (itemId, item) {
+        return $http.put(getUrlForId(itemId), item);
+    };
 
-        dashboard.items = [];
+    service.destroy = function (itemId) {
+        return $http.delete(getUrlForId(itemId));
+    };
+})
+
+.controller('LoginCtrl', function($rootScope, $state, Backand){
+    var login = this;
+
+    function signin() {
+        Backand.signin(login.email, login.password, login.appName)
+            .then(function() {
+                $rootScope.$broadcast('authorized');
+                $state.go('dashboard');
+            }, function(error) {
+                console.log(error);
+            });
+    }
+
+    login.newUser = false;
+    login.signin = signin;
+})
+
+.controller('MainCtrl', function ($rootScope, $state, Backand) {
+    var main = this;
+
+    function logout() {
+        Backand.signout()
+            .then(function(){
+                $state.go('login');
+            })
+    }
+
+    $rootScope.$on('unauthorized', function() {
+        $state.go('login');
+    });
+
+    main.logout = logout;
+})
+
+.controller('DashboardCtrl', function(ItemsModel){
+    var dashboard = this;
+
+    function getItems() {
+        ItemsModel.all()
+            .then(function (result) {
+                dashboard.items = result.data.data;
+            });
+    }
+
+    function createItem(item) {
+        ItemsModel.create(item)
+            .then(function (result) {
+                initCreateForm();
+                getItems();
+            });
+    }
+
+    function updateItem(item) {
+        ItemsModel.update(item.Id, item)
+            .then(function (result) {
+                cancelEditing();
+                getItems();
+            });
+    }
+
+    function deleteItem(itemId) {
+        ItemsModel.destroy(itemId)
+            .then(function (result) {
+                cancelEditing();
+                getItems();
+            });
+    }
+
+    function initCreateForm() {
+        dashboard.newItem = { name: '', description: '' };
+    }
+
+    function setEditedItem(item) {
+        dashboard.editedItem = angular.copy(item);
+        dashboard.isEditing = true;
+    }
+
+    function isCurrentItem(itemId) {
+        return dashboard.editedItem !== null && dashboard.editedItem.Id === itemId;
+    }
+
+    function cancelEditing() {
         dashboard.editedItem = null;
         dashboard.isEditing = false;
-        dashboard.getItems = getItems;
-        dashboard.createItem = createItem;
-        dashboard.updateItem = updateItem;
-        dashboard.deleteItem = deleteItem;
-        dashboard.setEditedItem = setEditedItem;
-        dashboard.isCurrentItem = isCurrentItem;
-        dashboard.cancelEditing = cancelEditing;
+    }
 
-        initCreateForm();
-        getItems();
-    })
-;
+    dashboard.items = [];
+    dashboard.editedItem = null;
+    dashboard.isEditing = false;
+    dashboard.getItems = getItems;
+    dashboard.createItem = createItem;
+    dashboard.updateItem = updateItem;
+    dashboard.deleteItem = deleteItem;
+    dashboard.setEditedItem = setEditedItem;
+    dashboard.isCurrentItem = isCurrentItem;
+    dashboard.cancelEditing = cancelEditing;
+
+    initCreateForm();
+    getItems();
+});
